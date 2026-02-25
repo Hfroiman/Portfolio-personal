e89b145a2624299402bf815baf3037683f69e721
Hfroiman <hernan.froiman@alumnos.frgp.utn.edu.ar>
2026-02-25 19:02:33 -0300

    1.8

--- src/Contacto/Contacto.js
+++ src/Contacto/Contacto.js
@@ -1,5 +1,5 @@
 const links = [
-    {url:"https://drive.google.com/file/d/10VMLUXd6dvWZEVbQCifHatu5788WzYwm/view", link:"Curriculum"},
+    {url:"https://drive.google.com/file/d/1vdURPpqQisQxhEJ34uUiJ9Tjo9Tc4WOI/view?usp=sharing", link:"Curriculum"},
     {url:"https://www.linkedin.com/in/hern%C3%A1nfroiman/", link:"linkedin"},
     {url:"https://github.com/Hfroiman?tab=repositories", link:"github"}
 ]
